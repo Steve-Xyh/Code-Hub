335d264288506727c659dd7f49c4f9f9c70e4e1f
stevexyh@hotmail.com <stevexyh@hotmail.com>
2019-09-09 01:35:57 +0800

    Ordinary Sync

--- Python/autoSubmit.py
+++ Python/autoSubmit.py
@@ -4,16 +4,20 @@
 """
 自动填表
 """
+import datetime
+
 from selenium import webdriver
 from selenium.common.exceptions import *
 from time import sleep
 
+
 name = input('姓名:')
 idNum = input('身份证:')
 telNum = input('手机号:')
-
-d = webdriver.Chrome()
-
+'''
+setTime = datetime.datetime(2019,9,8,23,39,30,0)
+endTime = datetime.datetime(2019,9,8,23,40,30,0)
+'''
 def submitinfo(url):
     d.get(url)
     try:
@@ -21,16 +25,34 @@ def submitinfo(url):
         d.find_element_by_id('sn').send_keys(idNum)
         d.find_element_by_id('tel').send_keys(telNum)
         d.find_element_by_class_name('g-btn').click()
+        print('Submit successfully on ' + url)
     except NoSuchElementException:
+        #sleep(1)
+        pass
+
+
+def autoSubmit():    
+    while True:
+        submitinfo('http://1.duotucms.com')
+        submitinfo('http://2.duotucms.com')
+        submitinfo('http://4.duotucms.com')
+        submitinfo('http://5.duotucms.com')
+        submitinfo('http://6.duotucms.com')
+        submitinfo('http://7.duotucms.com')
+
+
+if __name__ == '__main__':
+    while True:
+        #currentTime = datetime.datetime.now()
+        #if currentTime >= setTime:
+        opt = webdriver.ChromeOptions()
+        opt.add_argument('--headless')
+        #d = webdriver.Chrome(options=opt)
+        d = webdriver.Chrome()
+        autoSubmit()
+        #else:
+        print(setTime - currentTime)
         sleep(1)
-
-while True:
-    submitinfo('http://1.duotucms.com')
-    submitinfo('http://2.duotucms.com')
-    submitinfo('http://4.duotucms.com')
-    submitinfo('http://5.duotucms.com')
-    submitinfo('http://6.duotucms.com')
-    submitinfo('http://7.duotucms.com')
-    pass
-
-d.close()
+        #if currentTime >= endTime:
+        #    break
+    d.close()
